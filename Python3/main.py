ed1b048a52f33ccd808f7502cb17f18c94378423
nuomi1 <nuomi1@qq.com>
2022-06-26 21:02:15 +0800

    feat: get and print weather

--- Python3/main.py
+++ Python3/main.py
@@ -1,16 +1,17 @@
-# 这是一个示例 Python 脚本。
+from get_availability import *
+from get_token import *
+from get_weather import *
 
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    token: str = get_token()
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    country: str = 'US'
+    language: str = 'en-US'
+    latitude: float = 40.73061
+    longitude: float = -73.935242
+
+    availability: dict = get_availability(token=token, latitude=latitude, longitude=longitude, country=country)
+    weather: dict = get_weather(token=token, language=language, latitude=latitude, longitude=longitude)
+
+    print(availability)
+    print(weather)
