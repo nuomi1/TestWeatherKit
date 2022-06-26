9afc6fb5a3e77e21f30dbe87e6e939178bf0a9d7
nuomi1 <nuomi1@qq.com>
2022-06-26 19:51:37 +0800

    feat: tuist init

---
+++ Swift/Targets/TestWeatherKit/Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+import TestWeatherKitKit
+import TestWeatherKitUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        TestWeatherKitKit.hello()
+        TestWeatherKitUI.hello()
+
+        return true
+    }
+
+}
