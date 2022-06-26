e39a8cf9281a5b8772b101fd37f77031c01fac15
nuomi1 <nuomi1@qq.com>
2022-06-26 20:12:47 +0800

    feat: TestWeatherKitApp

---
+++ Swift/Targets/TestWeatherKit/Sources/TestWeatherKitApp.swift
@@ -0,0 +1,22 @@
+//
+//  TestWeatherKitApp.swift
+//  TestWeatherKit
+//
+//  Created by nuomi1 on 2022/6/11.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct TestWeatherKitApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+                    .navigationTitle("TestWeatherKit")
+            }
+        }
+    }
+}
